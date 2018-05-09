08e102086e1b1d58a1075c94d28a0a97bf5db843
Karol Krizka <kkrizka@gmail.com>
2018-05-09 13:41:07 -0700

    Add documentation to endeavour_master.

--- vhdl/endeavour_master.vhd
+++ vhdl/endeavour_master.vhd
@@ -1,6 +1,33 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+----
+-- Entity: endeavour_master
+-- Author: Karol Krizka <kkrizka@gmail.com>
+--
+-- Entity responsible for acting as a master in the Endeavour protocol. The entity is very dumb, with the
+-- software responsible for most of the work. It acts only as a (de)serializer for the morse code. The Tx
+-- and Rx parts kept separate. This is to keep it simple and generic.
+--
+--Generic ports
+-- clock - Clock used for internal logic and to time the serial line (nominal 80 MHz)
+-- reset - Active high reset signal to reset internal state machines
+-- serialin - The data sent from the the Endeavour slave
+-- serialout - The data sent to the the Endeavour slave
+--
+--Transfer ports
+--
+-- nbitsin - Number of bits to transfer from the datain
+-- datain - The data to transfer. The least significant nbitsin will be send, starting with bit at nbitsin-1.
+-- send - Pulse to send data stored in datain.
+-- busy - Indicates that the Tx FSM is sending data. Any calls to send will be ignored.
+--
+--Receive ports
+-- nbitsout - Number of valid bits received from the slave.
+-- dataout - The data recieved from the slave, stored in the nbitsout least significant bits. The bit at nbitsout-1 was recieved first.
+-- datavalid - High signal indicates that dataout contains valid and complete data. Goes low after reset and when a new word is being serialized.
+-- error - Inidicates an error condition during serialization of serialin. Currently only the length of a pulse must be in the specified number of clock cycles.
+----
 entity endeavour_master is
   port (
     clock       : in  std_logic;
